47791c30697a9fceb8678879b77ddf03c990c13c
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-06 22:07:24 +0200

    creating app/auth/forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,11 +3,13 @@ from wtforms import ValidationError,StringField,PasswordField,SubmitField,Boolea
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
+
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -16,9 +18,11 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
+
     def validate_email(self,data_field):
          if User.query.filter_by(email =data_field.data).first():
             raise ValidationError('There is an account with that email')
+        
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
