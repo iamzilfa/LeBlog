76a7ca3eb207e6ff46ff6c62bea0f484b64013ba
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-06 22:08:44 +0200

    creating app/auth/views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -38,5 +38,4 @@ def register():
         mail_message('Welcome to my personal blog','email/welcome_user',user.email,user = user)
         
         return redirect(url_for('auth.login'))
-        title = "New Account"
     return render_template('auth/register.html',registration_form = form)
