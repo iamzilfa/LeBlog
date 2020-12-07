36a555a3cd0b1312d7be225e9af1c4012e86b29d
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-07 13:05:03 +0200

    last

--- app/main/views.py
+++ app/main/views.py
@@ -50,10 +50,11 @@ def comment(blog_id):
     if form.validate_on_submit():
         comment = form.comment.data
         blog_id = blog_id
-        new_comment = Comment(comment = comment,blog_id=blog_id)
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,blog_id=blog_id)
         new_comment.save_comments()
 
-        return redirect(url_for('.comment',blog_id = blog_id))
+        return redirect(url_for('.comment',user_id = user_id,blog_id = blog_id))
 
     return render_template('comment.html',form = form,blogs = blogs, comments = comments)
 
