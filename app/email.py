e7303bed8b3da03fcb279009b9edd74c831f4469
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-10 09:31:18 +0200

    changes

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'iamzilfa@gmail.com'
+    sender_email = 'webfullstacker@gmail.com'
 
 
     email = Message(subject, sender=sender_email, recipients=[to])
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY=12345
 
 
-export MAIL_USERNAME=iamzilfa@gmail.com
+export MAIL_USERNAME=webfullstacker@gmail.com
 export MAIL_PASSWORD=kigali123!
 
 python3.6 manage.py server
